688a18b1253e8e3b343471b8298551db414657be
dallopez <70530126+dallopez@users.noreply.github.com>
2021-02-11 20:43:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.body.querySelector("#cool").innerHTML="My Name is"
+var prompter = prompt("what is your name?");
+document.body.querySelector("#lessCool").innerHTML=prompter;
+
+document.body.querySelector("#dope").innerHTML="and my fave color is"
+var prompter = prompt("what is your fav color?");
+document.body.querySelector("#lessDope").innerHTML=prompter;
